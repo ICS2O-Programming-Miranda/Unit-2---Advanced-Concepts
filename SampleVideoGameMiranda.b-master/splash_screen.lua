d59ed34b4db5d7afdbfd91dfdf8cca6358056500
Miranda Bennett <miranda.bennett@ocsbstudent.ca>
2020-04-22 10:38:16 -0400

    added to sample video game

--- SampleVideoGameMiranda.b-master/splash_screen.lua
+++ SampleVideoGameMiranda.b-master/splash_screen.lua
@@ -1,8 +1,8 @@
 -----------------------------------------------------------------------------------------
 --
 -- splash_screen.lua
--- Created by: Your Name
--- Date: Month Day, Year
+-- Created by:Miranda.B
+-- Date: April 17, 2020
 -- Description: This is the splash screen of the game. It displays the 
 -- company logo that...
 -----------------------------------------------------------------------------------------
@@ -23,9 +23,12 @@ local scene = composer.newScene( sceneName )
 -----------------------------------------------------------------------------------------
  
 -- The local variables for this scene
-local beetleship
+local star
 local scrollXSpeed = 8
 local scrollYSpeed = -3
+----------------------------------------------------------------------------------------
+-- SOUNDS
+-----------------------------------------------------------------------------------------
 local jungleSounds = audio.loadSound("Sounds/animals144.mp3")
 local jungleSoundsChannel
 
@@ -33,10 +36,12 @@ local jungleSoundsChannel
 -- LOCAL FUNCTIONS
 --------------------------------------------------------------------------------------------
 
--- The function that moves the beetleship across the screen
-local function moveBeetleship()
-    beetleship.x = beetleship.x + scrollXSpeed
-    beetleship.y = beetleship.y + scrollYSpeed
+-- The function that moves the star across the screen
+local function moveStar()
+    star.x = star.x - scrollXSpeed
+    star.y = star.y - scrollYSpeed
+     -- make the star fade out
+    star.alpha = star.alpha - 0.001
 end
 
 -- The function that will go to the main menu 
@@ -57,15 +62,18 @@ function scene:create( event )
     -- set the background to be black
     display.setDefault("background", 0, 0, 0)
 
-    -- Insert the beetleship image
-    beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
+    -- Insert the star image
+    star = display.newImageRect("Images/star.png", 200, 200)
 
-    -- set the initial x and y position of the beetleship
-    beetleship.x = 100
-    beetleship.y = display.contentHeight/2
+    -- set the initial x and y position of the star
+    star.x = 1024
+    star.y = display.contentHeight/2
+
+    --set star to full colour
+    star.alpha = 1
 
     -- Insert objects into the scene group in order to ONLY be associated with this scene
-    sceneGroup:insert( beetleship )
+    sceneGroup:insert( star )
 
 end -- function scene:create( event )
 
@@ -93,7 +101,7 @@ function scene:show( event )
         jungleSoundsChannel = audio.play(jungleSounds )
 
         -- Call the moveBeetleship function as soon as we enter the frame.
-        Runtime:addEventListener("enterFrame", moveBeetleship)
+        Runtime:addEventListener("enterFrame", moveStar)
 
         -- Go to the main menu screen after the given time.
         timer.performWithDelay ( 3000, gotoMainMenu)          
